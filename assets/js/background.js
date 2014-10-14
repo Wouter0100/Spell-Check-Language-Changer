742f52f8c31c518ac2019e542ef0b488c33bdb30
Wouter0100 <wouter@wouter0100.nl>
2014-10-14 20:21:45 +0200

    Created inject script for the chrome://settings-frame/languages page to update lang.

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,15 +1,7 @@
-var SPELL_CHECK_DICTIONARY_PREF = 'spellcheck.dictionary';
-
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     console.log(changeInfo);
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-    setLanguage('nl');
+
 });
-
-function setLanguage(language) {
-    chrome.send('setStringPref', [ SPELL_CHECK_DICTIONARY_PREF, String(language) ]);
-
-    chrome.send('spellCheckLanguageChange', [language]);
-}
